2211cfa901b97b52c1a12460f53b14bde542bb42
RobAngel28 <raalvarez@gbox.adnu.edu.ph>
2025-08-08 15:42:37 +0800

    Alvarezmain.cpp

--- Alvarezmain.cpp
+++ Alvarezmain.cpp
@@ -9,7 +9,7 @@ int main()
    cin >> num1;                                                                 // user input number
    cout << "Another number: ";                                                  //output another number
    cin >> num2;                                                                 //user input another number
-   product = num1 * num2;                                                       //set the value of sum as num1 + num2
+   product = num1 * num2;                                                       //set the value of product as num1 * num2
    cout << "The product of the wro numbes is: " << product;                     //output sum of numbers
     
     return 0;
